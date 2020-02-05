92eafaa9e0c36584ec2917279dc2c6ee23af94ca
Alan Norton <me@alannorton.com>
2020-02-05 14:59:23 -0500

    modify lab2 for coraz7

--- lab2/lab2.srcs/sources_1/new/z1top.v
+++ lab2/lab2.srcs/sources_1/new/z1top.v
@@ -1,11 +1,9 @@
 `timescale 1ns / 1ps
 
 module z1top(
-  input CLK_125MHZ_FPGA,
+  input CLOCK,
   input [3:0] BUTTONS,
-  input [1:0] SWITCHES,
   output [5:0] LEDS
 );
-  and(LEDS[0], BUTTONS[0], SWITCHES[0]);
-  assign LEDS[5:1] = 0;
+  and(LEDS[2], BUTTONS[0], BUTTONS[1]);
 endmodule
