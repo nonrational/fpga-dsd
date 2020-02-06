81712b0d1076f4e10a68db6081a690989b479b37
Alan Norton <me@alannorton.com>
2020-02-06 15:24:27 -0500

    i'm close, i can feel it

--- lab2/lab2.srcs/sources_1/new/structural_adder.v
+++ lab2/lab2.srcs/sources_1/new/structural_adder.v
@@ -3,5 +3,26 @@ module structural_adder (
     input [13:0] b,
     output [14:0] sum
 );
-    assign sum = 15'd0;
+
+    genvar i;
+
+    full_adder f0 (
+        .a(a[0]),
+        .b(b[0]),
+        .carry_in(0),
+        .sum(sum[0]),
+        .carry_out(sum[1])
+    );
+
+    generate
+        for (i = 1; i <= 13 ; i = i + 1) begin:twobitadder
+            full_adder genadder (
+                .a(a[i]),
+                .b(b[i]),
+                .carry_in(sum[i-1]),
+                .sum(sum[i]),
+                .carry_out(sum[i+1])
+            );
+        end
+    endgenerate
 endmodule
--- lab2/lab2.srcs/sources_1/new/z1top_adder.v
+++ lab2/lab2.srcs/sources_1/new/z1top_adder.v
@@ -5,13 +5,41 @@ module z1top_adder (
   input [1:0] BUTTONS,
   output [5:0] LEDS
 );
+  wire [11:0] ain = BUTTONS[0] == 1 ? 11'b00000000111 : 0;
+  wire [11:0] bin = BUTTONS[1] == 1 ? 11'b00000000111 : 0;
 
-  full_adder fa (
-    .a(BUTTONS[0]),
-    .b(BUTTONS[1]),
-    .carry_in(0),
-    .sum(LEDS[0]),
-    .carry_out(LEDS[3])
+  structural_adder user_adder (
+      .a(ain),
+      .b(bin),
+      .sum(LEDS[3:0])       // Upper bits will be truncated
   );
+
+  // // Self test of the structural adder
+  // wire [13:0] adder_operand1, adder_operand2;
+  // wire [14:0] structural_out, behavioral_out;
+  // wire test_fail;
+  // assign LEDS[4] = ~test_fail;
+  // assign LEDS[5] = ~test_fail;
+
+  // structural_adder structural_test_adder (
+  //     .a(adder_operand1),
+  //     .b(adder_operand2),
+  //     .sum(structural_out)
+  // );
+
+  // behavioral_adder behavioral_test_adder (
+  //     .a(adder_operand1),
+  //     .b(adder_operand2),
+  //     .sum(behavioral_out)
+  // );
+
+  // adder_tester tester (
+  //     .structural_sum(structural_out), // in
+  //     .behavioral_sum(behavioral_out), // in
+  //     .adder_operand1(adder_operand1), // out
+  //     .adder_operand2(adder_operand2), // out
+  //     .clk(CLOCK),                     // in
+  //     .test_fail(test_fail)            // out
+  // );
 
 endmodule
