69e104a6cd592d60218f5e48c02d9e0dda6b43e3
Alan Norton <me@alannorton.com>
2020-03-17 14:14:23 -0400

    fix top definition, fix cpu threads

--- skeleton-z7/src/skel_top.v
+++ skeleton-z7/src/skel_top.v
@@ -1,10 +1,9 @@
 `timescale 1ns/1ns
 
-module tone_top (
+module skel_top (
     input CLOCK,
     input [1:0] BUTTONS,
     output [5:0] LEDS
 );
-    // TODO(you): Your code here. Remove the following lines once you add your implementation.
     assign LEDS[5:0] = 6'b111111;
 endmodule
