e7fb24f2df6522a85c2fa76275aa09df4542dec5
Alan Norton <git@nonration.al>
2020-04-12 19:44:03 -0400

    use a limit register to control speed, rather than index width

--- skeleton-tinyfgpa/top.v
+++ skeleton-tinyfgpa/top.v
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 // look in pins.pcf for all the pin names on the TinyFPGA BX board
 module top (
     input CLK,     // 16MHz clock
@@ -11,59 +20,45 @@ module top (
     assign USBPU = 0;
     assign LED = 0;
 
-    lookup_blinker SOS (
+    tickspeed_blinker SOS (
         .CLK(CLK),
-        .blink_pattern(32'b10101000_11101110111000_10101000_00),
+        .blink_pattern(102'b101110101000_10111000_11101000_11101110111000_101000_111000_10111000_1011101000_11101000_11101110111000_11101000),
+
         .LED(PIN_13),
-        .blink_counter()
+        .blink_index()
     );
 endmodule
 
-module lookup_blinker
+module tickspeed_blinker
     #(
-        parameter MESSAGE_WIDTH=32,
-        parameter COUNTER_WIDTH=26,
-        parameter COUNTER_INDEX_WIDTH=5
+        parameter TICK_RATE='d5_000_000,
+        parameter MESSAGE_WIDTH=102
      )
 (
     input CLK,
     input [MESSAGE_WIDTH-1:0] blink_pattern,
 
-    output reg [COUNTER_WIDTH-1:0] blink_counter,
-    output LED
+    output LED,
+    output reg [$clog2(MESSAGE_WIDTH)-1:0] blink_index
 );
+
+    reg [31:0] tick;
+
     always @(posedge CLK)
     begin
-        blink_counter <= blink_counter + 1;
+        tick = tick + 1;
+        if (tick >= TICK_RATE)
+        begin
+            blink_index <= blink_index + 1;
+            tick = 0;
+        end
     end
 
     initial
         begin
-            blink_counter = 'b1;
+            blink_index = 'b1;
+            tick = 0;
         end
 
-    // "Bit extraction of var[31:0] requires 5 bit index, not 6 bits."
-    wire [COUNTER_INDEX_WIDTH-1:0] index;
-    //                              [26-1:26-5] => [25:21]
-    assign index = blink_counter[COUNTER_WIDTH-1:COUNTER_WIDTH-COUNTER_INDEX_WIDTH];
-    assign LED = blink_pattern[index];
+    assign LED = blink_pattern[blink_index];
 endmodule
-
-// module lookup_blinker
-//     #(parameter WIDTH=32)
-//     (
-//         input CLK,
-//         input [WIDTH-1:0] blink_pattern,
-//         output LED,
-//         output [25:0] blink_counter
-//     );
-
-//     initial blink_counter = 26'b00001000000000000000000000;
-//     always @(posedge CLK)
-//     begin
-//         blink_counter <= blink_counter + 1;
-//     end
-//     wire [4:0] index;
-//     assign index = blink_counter[25:21];
-//     assign LED = blink_pattern[index];
-// endmodule
--- skeleton-tinyfgpa/top_tb.v
+++ skeleton-tinyfgpa/top_tb.v
@@ -1,52 +1,46 @@
 `timescale 1ns/1ns
 
-`define STEP 4194304000
-`define MESSAGE_WIDTH 32
-`define COUNTER_WIDTH 26
-`define COUNTER_INDEX_WIDTH 5
-
 module top_tb ();
   reg clock;
   initial clock = 0;
-  always #1 clock <= ~clock;
+  always #4 clock <= ~clock;
 
   wire gr_led;
 
-  reg [MESSAGE_WIDTH-1:0] blink_pattern = 32'b01010101010101001111111111111111;
+  reg [31:0] blink_pattern = 32'b01010101010101001111111111111111;
+  wire [4:0] blink_index;
 
-  wire [COUNTER_WIDTH-1:0] blink_counter;
-  wire [COUNTER_INDEX_WIDTH-1:0] index;
-  assign index = blink_counter[COUNTER_WIDTH-1:COUNTER_WIDTH-COUNTER_INDEX_WIDTH];
-
-  lookup_blinker #(.MESSAGE_WIDTH(`MESSAGE_WIDTH), .COUNTER_WIDTH(`COUNTER_WIDTH), .COUNTER_INDEX_WIDTH(`COUNTER_INDEX_WIDTH)) blinky (
+  tickspeed_blinker #(.TICK_RATE(100), .MESSAGE_WIDTH(32)) blinky (
     .CLK(clock),
     .blink_pattern(blink_pattern),
 
     .LED(gr_led),
-    .blink_counter(blink_counter)
+    .blink_index(blink_index)
   );
 
   realtime last_flip_at;
   initial last_flip_at = 0;
 
+  // initial $monitor("[%t] %b", $time, blink_index);
+
   always begin
     @(gr_led) // on every flip, print how long since the last flip
       begin
-        $display("[%t] idx:%b pattern:%b gr_led:%b", $realtime - last_flip_at, index, blink_pattern[index], gr_led);
+        $display("[%t] gr_led:%b", $realtime - last_flip_at, gr_led);
         $fflush();
         last_flip_at = $realtime;
       end
   end
 
   always begin
-    #(`STEP) begin
+    #1 begin
       $fflush();
 
-      if (index == 0)
+      if (blink_index == 0)
         begin
-        $display("[%t] @%b -> %b", $realtime - last_flip_at, index, gr_led);
-        $fflush();
-        $finish();
+          $display("[%t] gr_led:%b", $realtime - last_flip_at, gr_led);
+          $fflush();
+          $finish();
         end
     end
   end
