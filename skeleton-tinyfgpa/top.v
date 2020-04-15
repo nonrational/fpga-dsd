839bdba5de0247db5cc2b348a33aebea416c60e9
Alan Norton <git@nonration.al>
2020-04-15 13:39:13 -0400

    click a piezo speaker along with lights

--- skeleton-tinyfgpa/top.v
+++ skeleton-tinyfgpa/top.v
@@ -5,19 +5,31 @@ module top (
     output LED,    // User/boot LED next to power LED
     output USBPU,  // USB pull-up resistor
 
+    output PIN_1,
     output PIN_13
 );
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
 
-    tickspeed_blinker #(.MESSAGE_WIDTH(84)) tsb (
+    tickspeed_blinker #(.MESSAGE_WIDTH(84)) tsb1 (
         .CLK(CLK),
         .blink_pattern(84'b0001_000000000_0001010111_000000000_00010111010111_000000000_000101010111_000000000_00011101),
 
         .LED(PIN_13),
-        .START(LED),
+        .START(),
         .blink_index()
     );
+
+    // tone_generator tg1 (
+    //     .clk(CLK),
+    //     .output_enable(),
+    //     .tone_switch_period(),
+    //     .volume(),
+
+    //     .square_wave_out(PIN_1)
+    // );
+
+    assign PIN_1 = PIN_13;
 endmodule
 
 module tickspeed_blinker
@@ -54,4 +66,30 @@ module tickspeed_blinker
         end
 
     assign LED = blink_pattern[blink_index];
+endmodule
+
+module tone_generator (
+    input        clk, // 125Mhz clock
+    input        output_enable, // ??
+    input [23:0] tone_switch_period, // ??
+    input        volume, // how to affect magnitue of wave?
+    output       square_wave_out // assign out to reg value for square wave
+);
+  localparam TONE_PERIOD_CLOCK_CYCLES = 72;
+
+  integer counter = 0;
+
+  initial square_wave_out = 1;
+
+  always @(posedge clk) begin
+    if (counter == 0)
+      begin
+          square_wave_out = ~square_wave_out;
+          counter = TONE_PERIOD_CLOCK_CYCLES;
+      end
+    else begin
+      if (counter > 0)
+        counter = counter - 1;
+    end
+  end
 endmodule
